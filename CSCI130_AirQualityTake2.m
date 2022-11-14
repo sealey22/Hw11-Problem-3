b272bcce8fd2eb736c7db492e1a117c242b8a9f5
Hsealey <112498796+sealey22@users.noreply.github.com>
2022-11-14 03:58:58 +0000

    displaying how many times the air was unsafe

---
+++ CSCI130_AirQualityTake2.m
@@ -0,0 +1,43 @@
+%% Table flavor of Air Quality Data
+% Instructions: use the import tool to read in the air quality data excel
+% file and read it in as a table. 
+load('aqidaily2020_COcities_Imported.mat')
+
+close all;
+
+yD = Data.PM2p5AQI_Denver;
+yF = Data.PM2p5AQI_FoCo;
+yG = Data.PM2p5AQI_GJ;
+
+figure(1)
+plot(yD, 'r*');
+hold on;
+plot(yF, 'bd');
+plot(yG, 'ko');
+legend('Denver', 'FoCo', 'GJ');
+xlabel('Day');
+ylabel('Particulate Level')
+grid on;
+
+%% Matrix flavor
+
+yMat(:,1) = Data.PM2p5AQI_Denver;
+yMat(:,2) = Data.PM2p5AQI_FoCo;
+yMat(:,3) = Data.PM2p5AQI_GJ;
+
+[r, c] = size(yMat);
+
+% color spec
+myRGB = [0 0 0; 0.1 0.7 0.6; 0.7 0.2 0.2];
+
+figure(2)
+for i = 1:c
+    plot(yMat(:,i),'*', 'Color',myRGB(i,:));
+    hold on;
+end
+legend('Denver', 'FoCo', 'GJ');
+xlabel('Day');
+ylabel('Particulate Level')
+grid on;
+
+
--- main.m
+++ main.m
@@ -1,1 +1,10 @@
 load('aqidaily2020_COcities_Imported.mat')
+%%This data corresponds to a time when there was rapid growth in fires in Colorado. In the editor, write a program to determine how many times the air quality was “Unhealthy for Sensitive Groups” for each city
+count = 0
+for i = 1:1:39
+ if PM2p5AQI_Denver(i) > 100
+total = count + 1
+disp(total)
+end
+end
+
